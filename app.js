014d65db0c40cb959f387922debda45c757c1595
mvento <markku.vento@edu.lapinamk.fi>
2025-09-22 05:29:29 +0300

    Tehtävä 3 tehty

--- app.js
+++ app.js
@@ -31,32 +31,70 @@ function toggleTheme() {
 themeBtn.addEventListener('click', toggleTheme);
 applyTheme(loadTheme());
 
-// 2) Haku — virhe: väärä API-osoite + virheenkäsittely puuttuu
+// 2) Haku — virhe: väärä API-osoite + virheenkäsittely puuttuu - Tämä tehty
 const form = document.getElementById('searchForm');
 const resultsEl = document.getElementById('results');
 const statusEl = document.getElementById('status');
 
+// Alustetaan controller muuttuja nulliksi
+let controller = null;
+
 // Coffee http-rajapinnan dokumentaatio: https://sampleapis.com/api-list/coffee
 async function searchImages(query) {
-    const url = `https://api.sampleapis.com/coffee/images`; // BUG: ei vastaa hakusanaan
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.slice(0, 8).map(x => ({ title: x.title || query, url: x.image }));
+    // Jos AbortController ei vielä määritetty tai edellinen signal on abortattu
+    if(controller == null || controller.signal.aborted) {
+        // Luodaan uusi AbortController
+        controller = new AbortController();
+    }
+    else {
+        // Muussa tapauksessa abortataan signal ja luodaan uusi AbortController
+        controller.abort();
+        controller = new AbortController();
+    }
+    // try/catch otettu käyttöön
+    try {
+        // Välitetään AbortController signal fetchiin
+        const resp = await fetch('https://api.sampleapis.com/coffee/hot', {
+            signal: controller.signal
+        });
+        const data = await resp.json();
+        // Määritetään haun status
+        statusEl.textContent = 'success';
+        // API ei tue query-parametria joten simuloidaan haku suodattamalla
+        // Määritetään ensin json-datasta data.slice
+        const datasliced = data.slice().map(x => ({ title: x.title, url: x.image }));
+        // Suodatetaan hakusanan perusteella (case sensitive)
+        const datafiltered = (datasliced.filter(function(item){
+            return item.title == query;         
+        }))
+        // Palautetaan suodatettu data
+        return datafiltered;
+    } 
+    catch(err) {
+        // Määritetään haun status virhesanomalla
+        statusEl.textContent = 'error (' + err.name + ')';
+    }
 }
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const q = $('#q').value.trim();
-    statusEl.textContent = 'Ladataan…';
-    const items = await searchImages(q); // BUG: ei try/catch, ks. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-    resultsEl.innerHTML = '';
-    items.forEach(item => {
-        const li = document.createElement('li');
-        li.className = 'card';
-        li.innerHTML = `<strong>${item.title}</strong><br><img alt="" width="160" height="120" src="${item.url}">`;
-        resultsEl.appendChild(li);
-    });
-    statusEl.textContent = `${items.length} tulosta`;
+    statusEl.textContent = 'Ladataan...';
+    $('#loader').style.display = "block"; // Näytetään latausspinneri
+    const items = await searchImages(q); // BUG: ei try/catch - Tämä korjattu
+    $('#loader').style.display = "none"; // Piilotetaan latausspinneri
+    // Ladataan sisältöä vain jos fetch onnistui
+    // (Tähän voisi keksiä paremman kuin string-tarkistuksen)
+    if(statusEl.textContent === 'success') {
+        resultsEl.innerHTML = '';
+        items.forEach(item => {
+            const li = document.createElement('li');
+            li.className = 'card';
+            li.innerHTML = `<strong>${item.title}</strong><br><img alt="" width="160" height="120" src="${item.url}">`;
+            resultsEl.appendChild(li);
+        });
+        statusEl.textContent = `${items.length} tulosta`;
+    }
 });
 
 // 3) Laskuri — virhe: event delegation ja bubbling sekoilee
